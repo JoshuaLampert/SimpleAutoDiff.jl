e10b4ff0ece41ae4d749fc0be1f3b19a7de0851a
Joshua Lampert <joshua.lampert@uni-hamburg.de>
2024-12-24 14:02:36 +0100

    add format check

--- src/dual_numbers.jl
+++ src/dual_numbers.jl
@@ -5,7 +5,7 @@ A struct representing a dual number ``a + εb``, where ``a`` and ``b`` are real 
 representing the `value` and the derivative `deriv` of a function in the context of automatic
 differentiation. Here, ``ε`` is a symbol satisfying ``ε^2 = 0``.
 """
-struct DualNumber{T <: Real} <: Number
+struct DualNumber{T<:Real} <: Number
     value::T
     deriv::T
 end
@@ -17,8 +17,8 @@ derivative(x::DualNumber) = x.deriv
 
 Base.real(::DualNumber{T}) where {T} = T
 
-Base.convert(::Type{DualNumber{T}}, x::Real) where {T <: Real} = DualNumber(x, zero(T))
-Base.promote_rule(::Type{DualNumber{T}}, ::Type{<:Real}) where {T <: Real} = DualNumber{T}
+Base.convert(::Type{DualNumber{T}}, x::Real) where {T<:Real} = DualNumber(x, zero(T))
+Base.promote_rule(::Type{DualNumber{T}}, ::Type{<:Real}) where {T<:Real} = DualNumber{T}
 
 # rules
 Base.:+(x::DualNumber, y::DualNumber) = DualNumber(x.value + y.value, x.deriv + y.deriv)
@@ -40,6 +40,6 @@ function Base.cos(x::DualNumber)
 end
 Base.log(x::DualNumber) = DualNumber(log(value(x)), derivative(x) / value(x))
 function Base.exp(x::DualNumber)
-   ex = exp(value(x))
-   return DualNumber(ex, ex * derivative(x))
+    ex = exp(value(x))
+    return DualNumber(ex, ex * derivative(x))
 end
